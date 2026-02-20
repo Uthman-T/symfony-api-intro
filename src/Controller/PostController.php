b69705f42874f40296464104a8647ab582deebfd
Uthman-T <wakfabrio15@gmail.com>
2026-02-20 15:55:05 +0100

    Config API client

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 final class PostController extends AbstractController
@@ -18,6 +18,8 @@ final class PostController extends AbstractController
 
         return $this->json([
             'posts' => $serializer->normalize($posts),
-        ]);
+        ],
+            Response::HTTP_OK,
+        );
     }
 }
